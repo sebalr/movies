0c7e2a9dc33b0fea25840c92f334af5721838ffa
Sebastian Larrieu <larrieu.sebastian@gmail.com>
2023-11-30 13:28:18 -0300

    movies components and routes

---
+++ src/app/movies/movie-detail/movie-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-movie-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './movie-detail.component.html',
+  styleUrls: ['./movie-detail.component.scss']
+})
+export class MovieDetailComponent {
+
+}
---
+++ src/app/movies/movies-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MovieDetailComponent } from 'src/app/movies/movie-detail/movie-detail.component';
+import { MoviesComponent } from 'src/app/movies/movies.component';
+
+const routes: Routes = [{
+  path: '',
+  component: MoviesComponent,
+},
+{
+  path: ':id',
+  component: MovieDetailComponent,
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MoviesRoutingModule { }
